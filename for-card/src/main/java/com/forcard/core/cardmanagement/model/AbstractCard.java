a5ebb018b2e2b103849821f3dc6ca41cff556709
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 17:31:35 +0100

    Implement abstract and domain model for card entity.

---
+++ for-card/src/main/java/com/forcard/core/cardmanagement/model/AbstractCard.java
@@ -0,0 +1,4 @@
+package com.forcard.core.cardmanagement.model;
+
+public abstract class AbstractCard {
+}
---
+++ for-card/src/main/java/com/forcard/core/cardmanagement/model/PaymentCard.java
@@ -0,0 +1,4 @@
+package com.forcard.core.cardmanagement.model;
+
+public class PaymentCard extends AbstractCard {
+}
