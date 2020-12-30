57eaf9ad230c282464045a4029d4bb384aaea54c
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 20:13:07 +0100

    Add annotation in PaymentCard.

--- for-card/src/main/java/com/forcard/core/cardmanagement/model/PaymentCard.java
+++ for-card/src/main/java/com/forcard/core/cardmanagement/model/PaymentCard.java
@@ -1,8 +1,12 @@
 package com.forcard.core.cardmanagement.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Getter
+@Setter
 @Document(collection = "payment-cards")
 public class PaymentCard extends AbstractCard {
 
