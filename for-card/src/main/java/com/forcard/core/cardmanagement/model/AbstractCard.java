3704d8b9286931014f08cc6f61c562fc55bec2a7
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 17:52:27 +0100

    Add fields in AbstractCard and PaymentCard models.

--- for-card/src/main/java/com/forcard/core/cardmanagement/model/AbstractCard.java
+++ for-card/src/main/java/com/forcard/core/cardmanagement/model/AbstractCard.java
@@ -1,4 +1,38 @@
 package com.forcard.core.cardmanagement.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public abstract class AbstractCard {
+
+    public static final String ID = "_id";
+    public static final String USER_ID = "userId";
+    public static final String EXPIRY_DATE = "expiryDate";
+    public static final String CREATED_DATE = "createdDate";
+    public static final String MODIFIED_DATE = "modifiedDate";
+
+    @Id
+    @Field(ID)
+    private ObjectId id;
+
+    @Field(USER_ID)
+    private String userId;
+
+    @Field(EXPIRY_DATE)
+    private LocalDate expiryDate;
+
+    @Field(CREATED_DATE)
+    private LocalDateTime createdDate;
+
+    @Field(MODIFIED_DATE)
+    private LocalDateTime modifiedDate;
+
 }
--- for-card/src/main/java/com/forcard/core/cardmanagement/model/PaymentCard.java
+++ for-card/src/main/java/com/forcard/core/cardmanagement/model/PaymentCard.java
@@ -1,4 +1,26 @@
 package com.forcard.core.cardmanagement.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "payment-cards")
 public class PaymentCard extends AbstractCard {
+
+    public static final String PAYMENT_CARD_PROVIDER = "paymentCardProvider";
+    public static final String PAYMENT_CARD_HOLDER = "paymentCardHolder";
+    public static final String PAYMENT_CARD_NUMBER = "paymentCardNumber";
+    public static final String CVV_CODE = "cvvCode";
+
+    @Field(PAYMENT_CARD_PROVIDER)
+    private String paymentCardProvider;
+
+    @Field(PAYMENT_CARD_HOLDER)
+    private String paymentCardHolder;
+
+    @Field(PAYMENT_CARD_NUMBER)
+    private String paymentCardNumber;
+
+    @Field(CVV_CODE)
+    private String cvvCode;
+
 }
