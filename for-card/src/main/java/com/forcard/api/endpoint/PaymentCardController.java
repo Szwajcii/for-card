395f906dd08b7a1d759e3c5fc4149d0c82dce8c9
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 18:16:22 +0100

    Implement descriptions for methods.

--- for-card/src/main/java/com/forcard/api/endpoint/PaymentCardController.java
+++ for-card/src/main/java/com/forcard/api/endpoint/PaymentCardController.java
@@ -1,5 +1,7 @@
 package com.forcard.api.endpoint;
 
+import com.forcard.core.cardmanagement.service.PaymentCardFindService;
+import com.forcard.core.cardmanagement.service.PaymentCardSaveService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/payment-card")
 public class PaymentCardController {
 
+    private static final String FIND_PAYMENT_CARD_BY_ID = "Find payment card by id";
+    private static final String FIND_ALL_PAYMENT_CARDS_BY_USER_ID = "Find all payment cards by user id";
+    private static final String ACTIVATE_PAYMENT_CARD = "Activate payment card";
+    private static final String CREATE_PAYMENT_CARD = "Create payment card";
+    private static final String UPDATE_PAYMENT_CARD = "Update payment card";
+    private static final String DELETE_PAYMENT_CARD_BY_ID = "Delete payment card by id";
+    private static final String VERIFY_PAYMENT_CARD_BY_CVV_CODE = "Verify payment card with cvv code";
 
+    private final PaymentCardFindService paymentCardFindService;
+    private final PaymentCardSaveService paymentCardSaveService;
 
+    public PaymentCardController(PaymentCardFindService paymentCardFindService,
+                                 PaymentCardSaveService paymentCardSaveService) {
+        this.paymentCardFindService = paymentCardFindService;
+        this.paymentCardSaveService = paymentCardSaveService;
+    }
 }
