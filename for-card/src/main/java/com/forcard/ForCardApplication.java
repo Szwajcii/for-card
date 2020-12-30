02cf9814403c21c937ee31dffd1d69be425bf35c
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 16:48:05 +0100

    Init commit.

---
+++ for-card/src/main/java/com/forcard/ForCardApplication.java
@@ -0,0 +1,13 @@
+package com.forcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForCardApplication.class, args);
+    }
+
+}
---
+++ for-card/src/test/java/com/forcard/ForCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
