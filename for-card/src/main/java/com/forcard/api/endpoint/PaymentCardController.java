646b61c50307dbb666dd34cc4ad580d1c933a970
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 17:27:35 +0100

    Implement PaymentCardController.

---
+++ for-card/src/main/java/com/forcard/api/endpoint/PaymentCardController.java
@@ -0,0 +1,9 @@
+package com.forcard.api.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment-card")
+public class PaymentCardController {
+}
