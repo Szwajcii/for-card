7c87b80183b3640fd266689c99541733a2bc3d33
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 19:27:47 +0100

    Implement InfoUtils.

---
+++ for-card/src/main/java/com/forcard/core/shared/utils/InfoUtils.java
@@ -0,0 +1,15 @@
+package com.forcard.core.shared.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InfoUtils {
+
+    public static final String NOT_FOUND = "Not found";
+
+    private InfoUtils() {
+    }
+
+}
