f8b04379062a353493bdbcbe0abec25e5b76fe25
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-30 20:16:43 +0100

    Add fields in response and request models.

--- for-card/src/main/java/com/forcard/api/rest/request/PaymentCardRequest.java
+++ for-card/src/main/java/com/forcard/api/rest/request/PaymentCardRequest.java
@@ -1,4 +1,21 @@
 package com.forcard.api.rest.request;
 
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class PaymentCardRequest {
+    private ObjectId id;
+    private String userId;
+    private String paymentCardProvider;
+    private String paymentCardHolder;
+    private String paymentCardNumber;
+    private LocalDate expiryDate;
+    private String cvvCode;
+    private boolean cardActive;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 }
--- for-card/src/main/java/com/forcard/api/rest/response/PaymentCardResponse.java
+++ for-card/src/main/java/com/forcard/api/rest/response/PaymentCardResponse.java
@@ -1,4 +1,20 @@
 package com.forcard.api.rest.response;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class PaymentCardResponse {
+    private String id;
+    private String userId;
+    private String paymentCardProvider;
+    private String paymentCardHolder;
+    private String paymentCardNumber;
+    private LocalDate expiryDate;
+    private String cvvCode;
+    private boolean cardActive;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 }
