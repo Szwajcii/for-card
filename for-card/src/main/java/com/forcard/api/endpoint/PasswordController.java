73cd255c1506699942515834b323583efd5bee8b
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-06 19:34:27 +0100

    Start implementing PasswordController.

---
+++ for-card/src/main/java/com/forcard/api/endpoint/PasswordController.java
@@ -0,0 +1,37 @@
+package com.forcard.api.endpoint;
+
+import com.forcard.core.usermanagement.service.PasswordService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/password")
+public class PasswordController {
+
+    private static final String CHANGE_PASSWORD = "Change user password.";
+    private static final String RESET_PASSWORD = "Reset user password.";
+    private static final String SAVE_PASSWORD = "Save user password.";
+
+    private final PasswordService passwordService;
+
+    public PasswordController(PasswordService passwordService) {
+        this.passwordService = passwordService;
+    }
+
+
+    @PreAuthorize("hasAnyAuthority('USER')")
+    @PostMapping("/reset-password")
+    @ApiOperation(value = RESET_PASSWORD, nickname = RESET_PASSWORD)
+    public ResponseEntity<String> resetPassword(HttpServletRequest request, @RequestParam("email") String email) {
+        return passwordService.resetUserPassword(request, email);
+    }
+
+
+}
