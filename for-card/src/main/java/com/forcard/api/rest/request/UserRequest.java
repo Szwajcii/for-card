dcdf656fe3f6010a65095910c159e22d89c3bc43
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-08 18:37:43 +0100

    Add user response and request models. Implement SuccessResponse model.

---
+++ for-card/src/main/java/com/forcard/api/rest/request/UserRequest.java
@@ -0,0 +1,17 @@
+package com.forcard.api.rest.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+
+@Getter
+@Setter
+public class UserRequest {
+    private ObjectId id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String phoneNumber;
+    private AddressRequest address;
+}
---
+++ for-card/src/main/java/com/forcard/api/rest/response/SuccessResponse.java
@@ -0,0 +1,14 @@
+package com.forcard.api.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessResponse {
+    private String message;
+}
