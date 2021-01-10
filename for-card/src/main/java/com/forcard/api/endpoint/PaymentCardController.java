e30d66fbb6c99c573dffda23f17faade78f3c83f
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-10 15:19:25 +0100

    Implement endpoints in PaymentCardController.

--- for-card/src/main/java/com/forcard/api/endpoint/PaymentCardController.java
+++ for-card/src/main/java/com/forcard/api/endpoint/PaymentCardController.java
@@ -1,9 +1,18 @@
 package com.forcard.api.endpoint;
 
+import com.forcard.api.rest.request.PaymentCardRequest;
+import com.forcard.api.rest.request.VerifyCardRequest;
+import com.forcard.api.rest.response.PaymentCardResponse;
+import com.forcard.api.rest.response.SuccessResponse;
 import com.forcard.core.cardmanagement.service.PaymentCardFindService;
 import com.forcard.core.cardmanagement.service.PaymentCardSaveService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.bson.types.ObjectId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/payment-card")
@@ -24,5 +33,54 @@ public class PaymentCardController {
                                  PaymentCardSaveService paymentCardSaveService) {
         this.paymentCardFindService = paymentCardFindService;
         this.paymentCardSaveService = paymentCardSaveService;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = FIND_PAYMENT_CARD_BY_ID)
+    public PaymentCardResponse findPaymentCardById(@PathVariable("id") String id) {
+        return paymentCardFindService.findPaymentCardById(new ObjectId(id));
+    }
+
+    @GetMapping("/all-payment-cards/{userId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = FIND_ALL_PAYMENT_CARDS_BY_USER_ID)
+    public Collection<PaymentCardResponse> findAllPaymentCardsByUserId(@PathVariable("userId") String userId) {
+        return paymentCardFindService.findAllPaymentCardsByUserId(userId);
+    }
+
+    @PostMapping("/activate-card/{paymentCardId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = ACTIVATE_PAYMENT_CARD)
+    public ResponseEntity<SuccessResponse> activatePaymentCard(@PathVariable("paymentCardId") String paymentCardId) {
+        return paymentCardSaveService.activatePaymentCard(paymentCardId);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = CREATE_PAYMENT_CARD)
+    public ResponseEntity<PaymentCardResponse> createPaymentCard(@RequestBody PaymentCardRequest paymentCardRequest) {
+        return paymentCardSaveService.createPaymentCard(paymentCardRequest);
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = UPDATE_PAYMENT_CARD)
+    public ResponseEntity<PaymentCardResponse> updatePaymentCard(@RequestBody PaymentCardRequest paymentCardRequest) {
+        return paymentCardSaveService.updatePaymentCard(paymentCardRequest);
+    }
+
+    @DeleteMapping("/delete/{paymentCardId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = DELETE_PAYMENT_CARD_BY_ID)
+    public ResponseEntity<SuccessResponse> deletePaymentCard(@PathVariable("paymentCardId") String paymentCardId) {
+        return paymentCardSaveService.deletePaymentCard(paymentCardId);
+    }
+
+    @PostMapping("/verify-payment-card")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @ApiOperation(value = VERIFY_PAYMENT_CARD_BY_CVV_CODE)
+    public ResponseEntity<?> verifyPaymentCard(@RequestBody VerifyCardRequest verifyCardRequest) {
+        return paymentCardFindService.verifyPaymentCard(verifyCardRequest);
     }
 }
