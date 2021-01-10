31301adb4c0d8d3ff99afbc27588c21498984235
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-10 15:18:14 +0100

    Implement methods to update user and to find user by id.

--- for-card/src/main/java/com/forcard/api/endpoint/UserController.java
+++ for-card/src/main/java/com/forcard/api/endpoint/UserController.java
@@ -1,13 +1,43 @@
 package com.forcard.api.endpoint;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.forcard.api.rest.request.UserRequest;
+import com.forcard.core.usermanagement.service.UserFindService;
+import com.forcard.core.usermanagement.service.UserSaveService;
+import io.swagger.annotations.ApiOperation;
+import org.bson.types.ObjectId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    private static final String FIND_USER_BY_ID = "Find user by id.";
     private static final String UPDATE_USER = "Update user profile.";
-    private static final String UPDATE_USER_ADDRESS = "Update user address.";
+
+    private final UserFindService userFindService;
+    private final UserSaveService userSaveService;
+
+
+    public UserController(UserFindService userFindService,
+                          UserSaveService userSaveService) {
+        this.userFindService = userFindService;
+        this.userSaveService = userSaveService;
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = FIND_USER_BY_ID, nickname = FIND_USER_BY_ID)
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    public ResponseEntity<?> findUserById(@PathVariable("id") String id) {
+        return userFindService.findUserById(new ObjectId(id));
+    }
+
+    @PutMapping
+    @ApiOperation(value = UPDATE_USER, nickname = UPDATE_USER)
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    public ResponseEntity<?> updateUser(@RequestBody UserRequest request) {
+        return userSaveService.updateUser(request);
+    }
 
 }
--- for-card/src/main/java/com/forcard/core/usermanagement/mapper/UserResponseMapper.java
+++ for-card/src/main/java/com/forcard/core/usermanagement/mapper/UserResponseMapper.java
@@ -1,4 +1,85 @@
 package com.forcard.core.usermanagement.mapper;
 
-public class UserResponseMapper {
+import com.forcard.api.rest.request.UserRequest;
+import com.forcard.api.rest.response.AddressResponse;
+import com.forcard.api.rest.response.UserResponse;
+import com.forcard.core.shared.model.Address;
+import com.forcard.core.shared.utils.MapperService;
+import com.forcard.core.usermanagement.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserResponseMapper implements MapperService<UserResponse, User> {
+
+
+    @Override
+    public UserResponse simplifyToRestObject(User domain) {
+        UserResponse userResponse = new UserResponse();
+        return mapFromDomainObject(domain, userResponse);
+    }
+
+    @Override
+    public User simplifyToDomainObject(UserResponse rest) {
+        User user = new User();
+        return mapToDomainObject(user, rest);
+    }
+
+    public UserResponse mapFromRestToResponse(UserRequest request) {
+        UserResponse response = new UserResponse();
+        if (request.getId() != null) {
+            response.setId(request.getId().toHexString());
+        }
+        response.setFirstName(request.getFirstName());
+        response.setLastName(request.getLastName());
+        response.setGender(request.getGender());
+        response.setEmail(request.getEmail());
+        response.setPhoneNumber(request.getPhoneNumber());
+        if (request.getAddress() != null) {
+            AddressResponse addressResponse = new AddressResponse(
+                request.getAddress().getStreet(),
+                request.getAddress().getNumber(),
+                request.getAddress().getFlat(),
+                request.getAddress().getTown(),
+                request.getAddress().getZipCode(),
+                request.getAddress().getCountry()
+            );
+            response.setAddress(addressResponse);
+        }
+        return response;
+    }
+
+    @Override
+    public UserResponse mapFromDomainObject(User domain, UserResponse rest) {
+        if (domain.getId() != null) {
+            rest.setId(domain.getId().toHexString());
+        }
+        rest.setFirstName(domain.getFirstName());
+        rest.setLastName(domain.getLastName());
+        rest.setGender(domain.getGender().name());
+        rest.setEmail(domain.getEmail());
+        rest.setPhoneNumber(domain.getPhoneNumber());
+        return rest;
+    }
+
+    @Override
+    public User mapToDomainObject(User domain, UserResponse rest) {
+        domain.setId(new ObjectId(rest.getId()));
+        domain.setFirstName(rest.getFirstName());
+        domain.setLastName(rest.getLastName());
+        domain.setEmail(rest.getEmail());
+        domain.setPhoneNumber(rest.getPhoneNumber());
+        if (rest.getAddress() != null) {
+            Address address = new Address(
+                rest.getAddress().getStreet(),
+                rest.getAddress().getNumber(),
+                rest.getAddress().getFlat(),
+                rest.getAddress().getTown(),
+                rest.getAddress().getZipCode(),
+                rest.getAddress().getCountry()
+            );
+            domain.setAddress(address);
+        }
+        return domain;
+    }
 }
