0db951df145f4677f515c224ad52f34e20ca1db5
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-11 08:54:18 +0100

    Implement MapperService methods in UserRequestMapper.

--- for-card/src/main/java/com/forcard/core/usermanagement/mapper/UserRequestMapper.java
+++ for-card/src/main/java/com/forcard/core/usermanagement/mapper/UserRequestMapper.java
@@ -1,4 +1,31 @@
 package com.forcard.core.usermanagement.mapper;
 
-public class UserRequestMapper {
+import com.forcard.api.rest.request.UserRequest;
+import com.forcard.core.shared.utils.MapperService;
+import com.forcard.core.usermanagement.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserRequestMapper implements MapperService<UserRequest, User> {
+    @Override
+    public UserRequest simplifyToRestObject(User domain) {
+        return null;
+    }
+
+    @Override
+    public User simplifyToDomainObject(UserRequest rest) {
+        return null;
+    }
+
+    @Override
+    public UserRequest mapFromDomainObject(User domain, UserRequest rest) {
+        return null;
+    }
+
+    @Override
+    public User mapToDomainObject(User domain, UserRequest rest) {
+        return null;
+    }
 }
