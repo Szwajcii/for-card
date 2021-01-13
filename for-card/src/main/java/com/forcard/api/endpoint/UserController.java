87001ee8c3fd60c519b8802e6c1fa2b727bda4a6
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-13 08:36:53 +0100

    Implement endpoint to get all users.

--- for-card/src/main/java/com/forcard/api/endpoint/UserController.java
+++ for-card/src/main/java/com/forcard/api/endpoint/UserController.java
@@ -9,10 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    private static final String FIND_ALL_USERS = "Find all users.";
     private static final String FIND_USER_BY_ID = "Find user by id.";
     private static final String UPDATE_USER = "Update user profile.";
 
@@ -24,6 +28,13 @@ public class UserController {
                           UserSaveService userSaveService) {
         this.userFindService = userFindService;
         this.userSaveService = userSaveService;
+    }
+
+    @GetMapping("/all")
+    @ApiOperation(value = FIND_ALL_USERS, nickname = FIND_ALL_USERS)
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public Collection<?> findAllUsers() {
+        return List.of();
     }
 
     @GetMapping("/{id}")
