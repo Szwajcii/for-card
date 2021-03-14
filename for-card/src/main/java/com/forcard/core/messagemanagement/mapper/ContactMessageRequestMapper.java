f0a987ef85a5be6ee9f9662b3a94a14e463eeca8
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-03-14 12:44:29 -0400

    Implement methods in contact message request and response mappers.

--- for-card/src/main/java/com/forcard/core/messagemanagement/mapper/ContactMessageRequestMapper.java
+++ for-card/src/main/java/com/forcard/core/messagemanagement/mapper/ContactMessageRequestMapper.java
@@ -2,6 +2,7 @@ package com.forcard.core.messagemanagement.mapper;
 
 import com.forcard.api.rest.request.ContactMessageRequest;
 import com.forcard.core.messagemanagement.model.ContactMessage;
+import com.forcard.core.messagemanagement.model.MessageStatus;
 import com.forcard.core.shared.utils.MapperService;
 import org.springframework.stereotype.Component;
 
@@ -10,21 +11,41 @@ public class ContactMessageRequestMapper implements MapperService<ContactMessage
 
     @Override
     public ContactMessageRequest simplifyToRestObject(ContactMessage domain) {
-        return null;
+        ContactMessageRequest request = new ContactMessageRequest();
+        return mapFromDomainObject(domain, request);
     }
 
     @Override
     public ContactMessage simplifyToDomainObject(ContactMessageRequest rest) {
-        return null;
+        ContactMessage domain = new ContactMessage();
+        return mapToDomainObject(domain, rest);
     }
 
     @Override
     public ContactMessageRequest mapFromDomainObject(ContactMessage domain, ContactMessageRequest rest) {
-        return null;
+        if (domain.getId() != null) {
+            rest.setId(domain.getId());
+        }
+        rest.setUserId(domain.getUserId());
+        rest.setName(domain.getName());
+        rest.setEmail(domain.getEmail());
+        rest.setMessage(domain.getMessage());
+        rest.setCreateDate(domain.getCreateDate());
+        rest.setStatus(domain.getStatus().name());
+        return rest;
     }
 
     @Override
     public ContactMessage mapToDomainObject(ContactMessage domain, ContactMessageRequest rest) {
-        return null;
+        if (rest.getId() != null) {
+            domain.setId(rest.getId());
+        }
+        domain.setUserId(rest.getUserId());
+        domain.setName(rest.getName());
+        domain.setEmail(rest.getEmail());
+        domain.setMessage(rest.getMessage());
+        domain.setCreateDate(rest.getCreateDate());
+        domain.setStatus(MessageStatus.valueOf(rest.getStatus()));
+        return domain;
     }
 }
--- for-card/src/main/java/com/forcard/core/messagemanagement/mapper/ContactMessageResponseMapper.java
+++ for-card/src/main/java/com/forcard/core/messagemanagement/mapper/ContactMessageResponseMapper.java
@@ -2,7 +2,9 @@ package com.forcard.core.messagemanagement.mapper;
 
 import com.forcard.api.rest.response.ContactMessageResponse;
 import com.forcard.core.messagemanagement.model.ContactMessage;
+import com.forcard.core.messagemanagement.model.MessageStatus;
 import com.forcard.core.shared.utils.MapperService;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,21 +12,37 @@ public class ContactMessageResponseMapper implements MapperService<ContactMessag
 
     @Override
     public ContactMessageResponse simplifyToRestObject(ContactMessage domain) {
-        return null;
+        ContactMessageResponse rest = new ContactMessageResponse();
+        return mapFromDomainObject(domain, rest);
     }
 
     @Override
     public ContactMessage simplifyToDomainObject(ContactMessageResponse rest) {
-        return null;
+        ContactMessage domain = new ContactMessage();
+        return mapToDomainObject(domain, rest);
     }
 
     @Override
     public ContactMessageResponse mapFromDomainObject(ContactMessage domain, ContactMessageResponse rest) {
-        return null;
+        rest.setId(domain.getId().toHexString());
+        rest.setUserId(domain.getUserId());
+        rest.setName(domain.getName());
+        rest.setEmail(domain.getEmail());
+        rest.setMessage(domain.getMessage());
+        rest.setCreateDate(domain.getCreateDate());
+        rest.setStatus(domain.getStatus().name());
+        return rest;
     }
 
     @Override
     public ContactMessage mapToDomainObject(ContactMessage domain, ContactMessageResponse rest) {
-        return null;
+        domain.setId(new ObjectId(rest.getId()));
+        domain.setUserId(rest.getUserId());
+        domain.setName(rest.getName());
+        domain.setEmail(rest.getEmail());
+        domain.setMessage(rest.getMessage());
+        domain.setCreateDate(rest.getCreateDate());
+        domain.setStatus(MessageStatus.valueOf(rest.getStatus()));
+        return domain;
     }
 }
