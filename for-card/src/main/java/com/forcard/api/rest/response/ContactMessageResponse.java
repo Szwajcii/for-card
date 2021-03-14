777e09c3d4333f6eaa391cc32dc36ea3171109ec
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-03-14 12:44:49 -0400

    Add fields in ContactMessageResponse object.

--- for-card/src/main/java/com/forcard/api/rest/response/ContactMessageResponse.java
+++ for-card/src/main/java/com/forcard/api/rest/response/ContactMessageResponse.java
@@ -3,7 +3,16 @@ package com.forcard.api.rest.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ContactMessageResponse {
+    private String id;
+    private String userId;
+    private String name;
+    private String email;
+    private String message;
+    private LocalDateTime createDate;
+    private String status;
 }
